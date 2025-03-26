fe6460e7fabd35462e76cb60f289f12d2d3846df
alevidals <alejandro.vidal.sanchez16@gmail.com>
2025-03-26 18:33:39 +0100

    feat: add navbar and social links on portfolio header

--- src/app/view/[username]/page.tsx
+++ src/app/view/[username]/page.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/app/view/_components/header";
 import { getPortfolioInfo } from "@/lib/db/queries/users";
 import { formatDate } from "@/lib/utils";
 import { redirect } from "next/navigation";
@@ -14,90 +15,113 @@ export default async function ViewPage({ params }: Props) {
     redirect("/");
   }
 
+  const socialLinks = {
+    github: portfolioInformation.githubUrl,
+    linkedin: portfolioInformation.linkedinUrl,
+    email: portfolioInformation.contactEmail,
+  };
+
   return (
-    <main className="mt-6 grid gap-10">
-      <p className="text-base">{portfolioInformation?.biography}</p>
-      <section>
-        <header>
-          <h2 className="text-xl font-bold mb-8 underline underline-offset-8">
-            Experiences
-          </h2>
-        </header>
-        <ul className="flex flex-col gap-6 border-l border-muted-foreground pl-5 py-1">
-          {portfolioInformation?.workExperiences.map((experience) => (
-            <li key={experience.id} className="relative">
-              <div className="absolute left-[-26px] top-[3px] size-3 rounded-full border border-muted-foreground bg-muted" />
-              <div className="text-sm text-muted-foreground mb-2">
-                <time dateTime={experience.startDate}>
-                  {formatDate({
-                    date: experience.startDate,
-                  })}
-                </time>
-                <span> - </span>
-                <time dateTime={experience.endDate || new Date().toISOString()}>
-                  {experience.endDate
-                    ? formatDate({
-                        date: experience.endDate,
-                      })
-                    : "Present"}
-                </time>
-              </div>
-              <h3 className="text-lg font-bold mb-1">{experience.position}</h3>
-              <p className="text-muted-foreground mb-2">
-                {experience.companyName}
-              </p>
-              <p className="text-sm">
-                As a full-stack developer at Innovatech Solutions, I design and
-                implement end-to-end software systems that drive business growth
-                and enhance user experiences. My responsibilities include
-                building scalable backend APIs using Node.js and Python,
-                developing responsive front-end interfaces with React, and
-                managing cloud-based infrastructure on AWS. I have successfully
-                led the migration of legacy systems to microservices
-                architecture, improving system performance by 40%. Additionally,
-                I collaborate with cross-functional teams to integrate CI/CD
-                pipelines, ensuring seamless deployment and continuous delivery.
-                Passionate about clean code and innovation, I thrive on solving
-                complex problems and delivering high-quality solutions that
-                align with business goals.
-              </p>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section>
-        <header>
-          <h2 className="text-xl font-bold mb-8 underline underline-offset-8">
-            Educations
-          </h2>
-        </header>
-        <ul className="flex flex-col gap-6 border-l border-muted-foreground pl-5 py-1">
-          {portfolioInformation?.educations.map((education) => (
-            <li key={education.id} className="relative">
-              <div className="absolute left-[-26px] top-[3px] size-3 rounded-full border border-muted-foreground bg-muted" />
-              <div className="text-sm text-muted-foreground mb-2">
-                <time dateTime={education.startDate}>
-                  {formatDate({
-                    date: education.startDate,
-                  })}
-                </time>
-                <span> - </span>
-                <time dateTime={education.endDate || new Date().toISOString()}>
-                  {education.endDate
-                    ? formatDate({
-                        date: education.endDate,
-                      })
-                    : "Present"}
-                </time>
-              </div>
-              <h3 className="text-lg font-bold mb-1">{education.degree}</h3>
-              <p className="text-muted-foreground mb-2">
-                {education.institution}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </main>
+    <div className="py-6 max-w-4xl container mx-auto">
+      <Header socialLinks={socialLinks} />
+      <main className="mt-6 grid gap-10">
+        <p className="text-base">{portfolioInformation?.biography}</p>
+        <section>
+          <header>
+            <h2
+              id="experiences"
+              className="text-xl font-bold mb-8 underline underline-offset-8 scroll-mt-20"
+            >
+              Experiences
+            </h2>
+          </header>
+
+          <ul className="flex flex-col gap-6 border-l border-muted-foreground ml-2 pl-5 py-1">
+            {portfolioInformation?.workExperiences.map((experience) => (
+              <li key={experience.id} className="relative">
+                <div className="absolute left-[-26px] top-[3px] size-3 rounded-full border border-muted-foreground bg-muted" />
+                <div className="text-sm text-muted-foreground mb-2">
+                  <time dateTime={experience.startDate}>
+                    {formatDate({
+                      date: experience.startDate,
+                    })}
+                  </time>
+                  <span> - </span>
+                  <time
+                    dateTime={experience.endDate || new Date().toISOString()}
+                  >
+                    {experience.endDate
+                      ? formatDate({
+                          date: experience.endDate,
+                        })
+                      : "Present"}
+                  </time>
+                </div>
+                <h3 className="text-lg font-bold mb-1">
+                  {experience.position}
+                </h3>
+                <p className="text-muted-foreground mb-2">
+                  {experience.companyName}
+                </p>
+                <p className="text-sm">
+                  As a full-stack developer at Innovatech Solutions, I design
+                  and implement end-to-end software systems that drive business
+                  growth and enhance user experiences. My responsibilities
+                  include building scalable backend APIs using Node.js and
+                  Python, developing responsive front-end interfaces with React,
+                  and managing cloud-based infrastructure on AWS. I have
+                  successfully led the migration of legacy systems to
+                  microservices architecture, improving system performance by
+                  40%. Additionally, I collaborate with cross-functional teams
+                  to integrate CI/CD pipelines, ensuring seamless deployment and
+                  continuous delivery. Passionate about clean code and
+                  innovation, I thrive on solving complex problems and
+                  delivering high-quality solutions that align with business
+                  goals.
+                </p>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section>
+          <header>
+            <h2
+              id="educations"
+              className="text-xl font-bold mb-8 underline underline-offset-8 scroll-mt-20"
+            >
+              Educations
+            </h2>
+          </header>
+          <ul className="flex flex-col gap-6 border-l border-muted-foreground ml-2 pl-5 py-1">
+            {portfolioInformation?.educations.map((education) => (
+              <li key={education.id} className="relative">
+                <div className="absolute left-[-26px] top-[3px] size-3 rounded-full border border-muted-foreground bg-muted" />
+                <div className="text-sm text-muted-foreground mb-2">
+                  <time dateTime={education.startDate}>
+                    {formatDate({
+                      date: education.startDate,
+                    })}
+                  </time>
+                  <span> - </span>
+                  <time
+                    dateTime={education.endDate || new Date().toISOString()}
+                  >
+                    {education.endDate
+                      ? formatDate({
+                          date: education.endDate,
+                        })
+                      : "Present"}
+                  </time>
+                </div>
+                <h3 className="text-lg font-bold mb-1">{education.degree}</h3>
+                <p className="text-muted-foreground mb-2">
+                  {education.institution}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </div>
   );
 }
--- src/app/view/_components/header.tsx
+++ src/app/view/_components/header.tsx
@@ -1,18 +1,58 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconMail,
+} from "@tabler/icons-react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
-export function Header() {
+type Props = {
+  socialLinks: {
+    github: string | null;
+    linkedin: string | null;
+    email: string | null;
+  };
+};
+
+export function Header({ socialLinks }: Props) {
   const { username } = useParams();
 
   return (
-    <header className="container mx-auto sticky top-6 bg-background z-50">
+    <header className="container mx-auto sticky max-w-4xl top-0 bg-background z-50">
       <div className="h-12 flex items-center justify-between">
         <Link href="/">
           <h1 className="text-xl font-bold">{username}</h1>
         </Link>
-        <nav>nav links</nav>
+        <div className="flex items-center gap-4">
+          <nav className="flex">
+            <Button variant="link" className="text-base">
+              <Link href="#educations">Educations</Link>
+            </Button>
+            <Button variant="link" className="text-base">
+              <Link href="#experiences">Experiences</Link>
+            </Button>
+          </nav>
+          <div className="flex gap-4">
+            {socialLinks.github && (
+              <a href={socialLinks.github} target="_blank" rel="noreferrer">
+                <IconBrandGithub size={28} />
+              </a>
+            )}
+            {socialLinks.linkedin && (
+              <a href={socialLinks.linkedin} target="_blank" rel="noreferrer">
+                <IconBrandLinkedin size={28} />
+              </a>
+            )}
+            {socialLinks.email && (
+              <a href={`mailto:${socialLinks.email}`}>
+                <IconMail size={28} />
+              </a>
+            )}
+          </div>
+        </div>
       </div>
     </header>
   );
