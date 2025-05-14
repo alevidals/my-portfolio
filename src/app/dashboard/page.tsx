69fe408dbd58759ff487a222413bbe0e06d5877e
alevidals <alejandro.vidal.sanchez16@gmail.com>
2025-05-14 18:43:30 +0200

    feat: add more rules to biome and format all files

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function DashboardPage() {
-	return (
-		<main className="container mx-auto mt-4">
-			<h1>This is the dashboard page</h1>
-		</main>
-	);
+  return (
+    <main className="container mx-auto mt-4">
+      <h1>This is the dashboard page</h1>
+    </main>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default async function Home() {
-	return (
-		<main className="container mx-auto mt-4">
-			<h1 className="text-5xl font-extrabold">Welcome to MyPortfolio</h1>
-			<h2 className="text-2xl">
-				The place where you can create your portfolio and CV at the same time 🚀
-			</h2>
-		</main>
-	);
+  return (
+    <main className="container mx-auto mt-4">
+      <h1 className="text-5xl font-extrabold">Welcome to MyPortfolio</h1>
+      <h2 className="text-2xl">
+        The place where you can create your portfolio and CV at the same time 🚀
+      </h2>
+    </main>
+  );
 }
